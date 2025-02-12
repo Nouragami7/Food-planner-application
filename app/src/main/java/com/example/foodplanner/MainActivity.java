2fc74a140ed3500d477fbae09ff96c77688b71de
Nouragami7 <https://github.com/Nouragami7>
2025-02-12 18:04:14 +0200

    navigate through navigation bottom bar

--- app/src/main/java/com/example/foodplanner/MainActivity.java
+++ app/src/main/java/com/example/foodplanner/MainActivity.java
@@ -1,17 +1,81 @@
 package com.example.foodplanner;
 
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.view.View;
 
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.navigation.NavController;
+import androidx.navigation.fragment.NavHostFragment;
+import androidx.navigation.ui.NavigationUI;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.navigation.NavigationBarView;
+
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
 
 public class MainActivity extends AppCompatActivity {
-    private static final String TAG = "MainActivity";
+    private BottomNavigationView bottomNav;
+    private NavController navController;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
+
+        bottomNav = findViewById(R.id.bottomNavigationView);
+        NavHostFragment navHostFragment = (NavHostFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.nav_host_fragment);
+        navController = navHostFragment.getNavController();
+        NavigationUI.setupWithNavController(bottomNav, navController);
+
+
+        bottomNav.setOnItemSelectedListener(new NavigationBarView.OnItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                return handleBottomNavigation(item);
+            }
+        });
+
+        Set<Integer> visibleFragments = new HashSet<>(Arrays.asList(
+                R.id.homeFragment,
+                R.id.searchFragment,
+                R.id.favouriteFragment,
+                R.id.planFragment
+        ));
+
+        navController.addOnDestinationChangedListener((controller, destination, arguments) -> {
+            if (visibleFragments.contains(destination.getId())) {
+                bottomNav.setVisibility(View.VISIBLE);
+            } else {
+                bottomNav.setVisibility(View.GONE);
+            }
+        });
     }
+
+    private boolean handleBottomNavigation(MenuItem item) {
+        int itemId = item.getItemId();
+
+        if (itemId == R.id.homeFragment) {
+            navController.navigate(R.id.homeFragment);
+            return true;
+        } else if (itemId == R.id.favouriteFragment) {
+            navController.navigate(R.id.favouriteFragment);
+            return true;
+        } else if (itemId == R.id.planFragment) {
+            navController.navigate(R.id.planFragment);
+            return true;
+        }else if(itemId == R.id.searchFragment){
+            navController.navigate(R.id.searchFragment);
+            return true;
+        }
+        return false;
+    }
+
 }
--- app/src/main/java/com/example/foodplanner/controller/PlanFragment.java
+++ app/src/main/java/com/example/foodplanner/controller/PlanFragment.java
@@ -1,27 +1,23 @@
 package com.example.foodplanner.controller;
 
-import android.graphics.Color;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
-import androidx.appcompat.widget.SearchView;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.EditText;
 
 import com.example.foodplanner.R;
 
-public class SearchFragment extends Fragment {
-    SearchView searchView;
+public class PlanFragment extends Fragment {
 
-    public SearchFragment() {
+
+    public PlanFragment() {
         // Required empty public constructor
     }
-
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -33,14 +29,11 @@ public class SearchFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_search, container, false);
+        return inflater.inflate(R.layout.fragment_plan, container, false);
     }
 
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
-        searchView = view.findViewById(R.id.searchView);
-        EditText searchEditText = searchView.findViewById(androidx.appcompat.R.id.search_src_text);
-        searchEditText.setTextColor(Color.BLACK);
     }
 }
